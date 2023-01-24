9c5396be8be4cc02616166efad9f4991260435b0
Camilla Birkelund <camillabirkelund@hotmail.com>
2023-01-24 15:09:43 +0100

    Fixed the calculator log

---
+++ øving2/calculator/src/components/Keypad.vue
@@ -0,0 +1,18 @@
+<script setup>
+    import Numbers from './Numbers.vue';
+    import Operations from './Operations.vue';
+</script>
+
+<template>
+    <div class="keypad">
+        <Numbers />
+        <Operations />
+    </div>
+</template>
+
+<style scoped>
+    .keypad {
+        margin: 1px;
+        display: flex;
+    }
+</style>
